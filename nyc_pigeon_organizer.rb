e0bd2f807699dcade3ea6b3607d87f9535738b8c
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 01:25:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,5 +7,17 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
+  feature= final.keys
+  data[:color].each do |brd_color, name|
+    name.each do |brdName|
+      feature.each do |i|
+        if brdName == i
+          final[i][:color] << brd_color.to_s 
+        end
+      end
+    end
+  end
+  
+  
   return final
 end
