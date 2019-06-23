5ed2a16181425cf011b85f47def74548a98d426c
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 01:19:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,9 @@
 def nyc_pigeon_organizer(data)
-  newHash = {}
+  final = {}
   data.each do |info, value|
     value.each do |atribt,arr|
       arr.each do |name|
-        #if !newHash.has_key?(name)
-         # newHash[name]= {}
-        #end
-        
-        #if !newHash.has_key?(info)
-       newHash[name][info]= []
-       end
-        
-        if !newHash.has[name][info].include?(atribt)
-          newHash[name][info] << atribt.to_s 
-        end
+        final[name]= 
       
       end
     end
