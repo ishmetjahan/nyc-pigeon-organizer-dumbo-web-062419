6950ea0eae5dc31838fa23038657e49cf49327d2
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 01:16:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newHash = {}
+  data.each do |info, value|
+    value.each do |atribt,arr|
+      arr.each do |name|
+        #if !newHash.has_key?(name)
+         # newHash[name]= {}
+        #end
+        
+        #if !newHash.has_key?(info)
+       newHash[name][info]= []
+       end
+        
+        if !newHash.has[name][info].include?(atribt)
+          newHash[name][info] << atribt.to_s 
+        end
+      
+      end
+    end
+  end
+  newHas
 end
